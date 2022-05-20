37eb17a587e88a928a2af8c5543c2ae18d123c96
Fahima BOUMELLIL <fboumell@e3r3p10.clusters.42paris.fr>
2022-05-20 16:12:10 +0200

    inshallah le parsing est bon

--- check_map_content.c
+++ check_map_content.c
@@ -6,7 +6,7 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 21:08:18 by adaloui           #+#    #+#             */
-/*   Updated: 2022/05/20 15:40:12 by fboumell         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:11:19 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,27 +110,32 @@ int	check_right_hole(char *s, int size)
 	return (SUCCESS);
 }
 
-int	check_left_hole(char *s1, char *s2)
+int	check_walls_left(char **map)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	if (!s1 || !s2)
-		return (SUCCESS);
-	// while (s1[i] && s1[i] == ' ')
-	// 	i++;
-	// j = 0;
-	// while (s2[j] && j < i)
-	// {
-	// 	if (s2[j] == '0')
-	// 		return (FAILURE);
-	// 	j++;
-	// }
+	i = 1;
+	while (map[i])
+	{
+		if (map[i][0] == '0')
+			return (FAILURE);
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == '0')
+			{
+				if (map[i - 1][j] == ' ' || map[i + 1][j] == ' ')
+					return (FAILURE);
+			}
+			j++;
+		}
+		i++;
+	}
 	return (SUCCESS);
 }
 
-int	check_walls_left_right(char **map)
+int	check_walls_right(char **map)
 {
 	int	i;
 	int	size;
@@ -139,26 +144,15 @@ int	check_walls_left_right(char **map)
 	while (map[i])
 	{
 		// printf("i : %d\n", i);
-		if (map[i][0] == '0')
-			return (return_failure("Error\nThere is a hole on the left"));
-		if (check_left_hole(map[i]))
 		size = ft_strlen(map[i]);
 		if (map[i][size - 1] == '0')
 			return (return_failure("Error\nThere is a hole on the right"));
 		if (ft_strlen(map[i]) < ft_strlen(map[i + 1]))
-		{
 			if (check_right_hole(map[i + 1], ft_strlen(map[i]) - 1) == FAILURE)
 				return (return_failure("Error\nThere is a hole on the right"));
-			// if (check_left_hole(map[i], map[i + 1]) == FAILURE)
-			// 	return (return_failure("Error\nThere is a hole ont the left"));
-		}
 		if (ft_strlen(map[i]) > ft_strlen(map[i + 1]))
-		{
 			if (check_right_hole(map[i], ft_strlen(map[i + 1]) - 1) == FAILURE)
 				return (return_failure("Error\nThere is a hole on the right"));
-			// if (check_left_hole(map[i], map[i + 1]) == FAILURE)
-			// 	return (return_failure("Error\nThere is a hole ont the left"));
-		}
 		i++;
 	}
 	return (SUCCESS);
@@ -192,7 +186,9 @@ int	check_map_content_characters(t_data *data)
 {
 	if (check_walls_top_bottom(data->map->map_2) == FAILURE)
 		return (FAILURE);
-	if (check_walls_left_right(data->map->map_2) == FAILURE)
+	if (check_walls_left(data->map->map_2) == FAILURE)
+		return (return_failure("Error\nThere is a hole on the left"));
+	if (check_walls_right(data->map->map_2) == FAILURE)
 		return (FAILURE);
 	if (check_hole_inside_map(data->map->map_2) == FAILURE)
 		return (return_failure("Error\nThere's a hole inside the map."));
