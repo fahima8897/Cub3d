1b0e8f6f130e517891d16d9f2bc0f649b154de4e
Adnen ALOUI <adaloui@e3r3p11.clusters.42paris.fr>
2022-05-20 20:13:42 +0200

    Cub3d - function to get all map data in a struct NO LEAKS

--- get_all_map_info.c
+++ get_all_map_info.c
@@ -6,7 +6,7 @@
 /*   By: adaloui <adaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 19:33:44 by adaloui           #+#    #+#             */
-/*   Updated: 2022/05/20 20:00:01 by adaloui          ###   ########.fr       */
+/*   Updated: 2022/05/20 20:12:48 by adaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,32 +34,38 @@ t_map_info	*get_textures(char **map, t_map_info *map_info)
 	return (map_info);
 }
 
+void	get_color_cd_reduce(char **s_byspace, t_map_info *map_info)
+{
+	char	**s_bycomma;
+
+	if (ft_strncmp(s_byspace[0], "F", 2) == 0)
+	{
+		s_bycomma = ft_split(s_byspace[1], ',');
+		map_info->f_red = ft_atoi(s_bycomma[0]);
+		map_info->f_green = ft_atoi(s_bycomma[1]);
+		map_info->f_blue = ft_atoi(s_bycomma[2]);
+		free_tab(s_bycomma);
+	}
+	if (ft_strncmp(s_byspace[0], "C", 2) == 0)
+	{
+		s_bycomma = ft_split(s_byspace[1], ',');
+		map_info->c_red = ft_atoi(s_bycomma[0]);
+		map_info->c_green = ft_atoi(s_bycomma[1]);
+		map_info->c_blue = ft_atoi(s_bycomma[2]);
+		free_tab(s_bycomma);
+	}
+}
+
 t_map_info	*get_colors_cf(char **map, t_map_info *map_info)
 {
 	int		i;
 	char	**s_byspace;
-	char	**s_bycomma;
 
 	i = -1;
 	while (map[++i] && i < 6)
 	{
 		s_byspace = ft_split(map[i], ' ');
-		if (ft_strncmp(s_byspace[0], "F", 2) == 0)
-		{
-			s_bycomma = ft_split(s_byspace[1], ',');
-			map_info->f_red = ft_atoi(s_bycomma[0]);
-			map_info->f_green = ft_atoi(s_bycomma[1]);
-			map_info->f_blue = ft_atoi(s_bycomma[2]);
-			free_tab(s_bycomma);
-		}
-		if (ft_strncmp(s_byspace[0], "C", 2) == 0)
-		{
-			s_bycomma = ft_split(s_byspace[1], ',');
-			map_info->c_red = ft_atoi(s_bycomma[0]);
-			map_info->c_green = ft_atoi(s_bycomma[1]);
-			map_info->c_blue = ft_atoi(s_bycomma[2]);
-			free_tab(s_bycomma);
-		}
+		get_color_cd_reduce(s_byspace, map_info);
 		free_tab(s_byspace);
 	}
 	return (map_info);
